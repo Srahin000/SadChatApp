257f70e8547e8a16736e7498c84c13cf6c44fd01
Sadman Rahin <sadmanrahin16@gmail.com>
2024-10-20 19:15:44 -0400

    backend fixed

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,7 @@ const app = express();
 
 app.use(express.json())// to accept json data
 
-app.get('/', (req,res)=>{
-    res.send("API is Running")
-})
+
 
 app.use('/api/user',userRoutes)
 app.use('/api/chat',chatRoutes)
@@ -29,6 +27,8 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname1, "/frontend/build")));
     app.get("*", (req, res) =>{
         res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+        
+           
     })
 
 }else{
