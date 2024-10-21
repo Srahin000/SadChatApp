60e4d4794cad82a120178e4183e09078728c4cba
Sadman Rahin <sadmanrahin16@gmail.com>
2024-10-20 21:23:06 -0400

    haha

--- backend/server.js
+++ backend/server.js
@@ -25,23 +25,6 @@ const PORT = process.env.PORT;
 
 const server =  app.listen (PORT, console.log(`Server started on PORT ${PORT}`));
 
-//-------------------deployment--------------------
-const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname1, "/frontend/build")));
-    app.get("*", (req, res) =>{
-        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-        
-           
-    })
-
-}else{
-    app.get("/", (req, res) => {
-      res.send("API is running");
-    });
-}
-
-//-------------------deployment--------------------
 
 const io = require("socket.io")(server,{
     pingTimeout:60000,
@@ -80,7 +63,22 @@ socket.off("setup",()=>{
     console.log("USER DISCONNECTED");
     socket.leave(userData._id);
 })
+//-------------------deployment--------------------
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+    app.get("*", (req, res) =>{
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+        
+           
+    })
 
+}else{
+    app.get("/", (req, res) => {
+      res.send("API is running");
+    });
+}
 
+//-------------------deployment--------------------
 
 })
