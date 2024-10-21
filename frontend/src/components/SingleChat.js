4df5be1273365888e4adfa043a27d7ca4898a716
Sadman Rahin <sadmanrahin16@gmail.com>
2024-10-20 21:36:33 -0400

    idk

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -11,7 +11,7 @@ import io from "socket.io-client"
 import Lottie from 'react-lottie';
 import animationData from '../Animations/typing.json';
 
-const ENDPOINT = "http://localhost:5000"
+const ENDPOINT = "https://sadchatapp-1-oerj.onrender.com/"
 var socket, selectedChatCompare;
 
 const SingleChat = ({fetchAgain, setFetchAgain}) => {
